f71c440212a9596ebb52873d5f5bc2ddbdd222dd
Brian Marquis <rbmarquis.dev@gmail.com>
2019-08-05 21:27:17 -0500

    Update api route with controller

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,21 @@
 
 'use strict';
 
-var expect = require('chai').expect;
+// var expect = require('chai').expect;
+const apiController = require('../controllers/apiController');
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .get(apiController.getRecentThreads)
+    .post(apiController.createThread)
+    .put(apiController.reportThread)
+    .delete(apiController.deleteThread);
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .get(apiController.getFullThread)
+    .post(apiController.createReply)
+    .put(apiController.reportReply)
+    .delete(apiController.deleteReply);
 
 };
