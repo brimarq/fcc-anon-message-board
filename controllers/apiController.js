a29ae23565815515f7e7c5a14428fd45053fcfe9
Brian Marquis <rbmarquis.dev@gmail.com>
2019-08-06 20:01:15 -0500

    Add createReply function

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,7 +5,7 @@ async function getRecentThreads(req, res) {
   try {
     const threads = await Thread
       .find({ board: { $eq: board }})
-      .then(threads => threads.map(thread => thread.getPublicFields()));
+      .then(threads => threads.map(thread => thread));
 
     
 
@@ -24,7 +24,26 @@ function reportThread(req, res) {}
 
 function deleteThread(req, res) {}
 
-function createReply(req, res) {}
+async function createReply(req, res) {
+  const board = req.params.board.toLowerCase();
+  const { thread_id, text, delete_password } = req.body;
+  console.dir({ body: req.body }, { depth: null })
+  const reply = { text, delete_password };
+
+  try {
+    const updatedThread = await Thread
+      .findOneAndUpdate({ _id: { $eq: thread_id } }, { $push: { replies: reply } }, { new: true })
+      .then(thread => thread);
+
+    console.log({ updatedThread });
+
+    res.send(updatedThread);
+
+  } catch(err) {
+    throw err;
+  }
+  
+}
 
 function reportReply(req, res) {} 
 
