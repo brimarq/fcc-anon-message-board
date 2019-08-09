2804b13b5e9fa55327c638d17c5b8bd198e58e0c
Brian Marquis <rbmarquis.dev@gmail.com>
2019-08-09 14:47:46 -0500

    Add 'replycount' field to threads ret from getRecentThreads, required by board.html.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,10 +4,18 @@ async function getRecentThreads(req, res) {
   const board = req.params.board.toLowerCase();
   try {
     const threads = await Thread
-      .find({ board: { $eq: board }})
-      .then(threads => threads.map(thread => thread));
-
-    
+      .find(
+        { board: { $eq: board }}, 
+        null, 
+        { sort: { bumped_on: 'desc', }, limit: 10 }
+      )
+      .then(threads => threads.map(thread => {
+        thread = thread.getPublicFields();
+        thread.replycount = thread.replies.length;
+        thread.replies = thread.replies.sort((a, b) => b.created_on - a.created_on).slice(0, 3);
+        return thread;
+      }))
+      .catch(err => err);
 
     res.send(threads);
 
@@ -16,28 +24,56 @@ async function getRecentThreads(req, res) {
   }
 }
 
-function getFullThread(req, res) {}
+async function getFullThread(req, res) {
+  const { thread_id } = req.query;
+  try {
+    const thread = await Thread
+      .findById(thread_id)
+      .then(thread => thread.getPublicFields())
+      .catch(err => ({ err }));
 
-function createThread(req, res) {}
+    res.send(thread);
+
+  } catch(err) {
+    throw err;
+  }
+}
+
+async function createThread(req, res) {
+  // Two `board` variables?? req.params.board OR req.body.board??
+  const board = req.params.board.toLowerCase();
+  const { text, delete_password } = req.body;
+  const thread = new Thread({ board, text, delete_password });
+  try {
+    const query = await thread
+      .save()
+      .then(thread => `/b/${thread.board}`)
+      .catch(err => ({ err }));
+
+    query.err ? res.send(query.err) : res.redirect(query);
+
+  } catch(err) {
+    throw err;
+  }
+}
 
 function reportThread(req, res) {} 
 
 function deleteThread(req, res) {}
 
 async function createReply(req, res) {
-  const board = req.params.board.toLowerCase();
   const { thread_id, text, delete_password } = req.body;
-  console.dir({ body: req.body }, { depth: null })
-  const reply = { text, delete_password };
-
   try {
-    const updatedThread = await Thread
-      .findOneAndUpdate({ _id: { $eq: thread_id } }, { $push: { replies: reply } }, { new: true })
-      .then(thread => thread);
+    const query = await Thread
+      .findOneAndUpdate(
+        { _id: { $eq: thread_id } }, 
+        { $push: { replies: { text, delete_password } } }, 
+        { new: true }
+      )
+      .then(thread => `/b/${thread.board}/${thread._id}`)
+      .catch(err => ({ err }));
 
-    console.log({ updatedThread });
-
-    res.send(updatedThread);
+    query.err ? res.send(query.err) : res.redirect(query);
 
   } catch(err) {
     throw err;
